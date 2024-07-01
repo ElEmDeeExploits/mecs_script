1bddc979eb1f610f094cb0c2fcd7746e29bffb60
ElEmDeeExploits <128638831+ElEmDeeExploits@users.noreply.github.com>
2024-06-30 20:35:27 -0400

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,3 +1,5 @@
---Made by ElEmDee
+--Original by ElEmDee
+--Update by Hyptonic
 
+-- Solara Version
 loadstring(game:HttpGet('https://raw.githubusercontent.com/ElEmDeeExploits/mecs_script/main/mainscript.lua'))()
